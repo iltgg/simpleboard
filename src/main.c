4d402d0372f832349210c5ebc828fc249b6f4a63
Benjamin Low <benlow999@gmail.com>
2024-03-06 16:34:17 -0500

    use new config struct

--- src/main.c
+++ src/main.c
@@ -5,20 +5,20 @@
 #include <string.h>
 
 #define DEBUG 0
+#define DEBUG_CONF 0
 
 static const char *DEFAULT_NAME = "simpleboard";
-static int title = 1;
-static char *titleOverride = NULL;
 
 void setup(Config *config) {
-  for (int i = 0; i < config->preferenceCount; i++) {
-    if (!strcmp(config->preference[i].key, "preCommand")) {
-      system(config->preference[i].value);
-    }
+  if (strlen(config->preference.preCommand) > 0) {
+    system(config->preference.preCommand);
   }
 }
 
 FILE *getConfigFile() {
+#if DEBUG_CONF == 1
+  return fopen("./simpleboard.conf", "r");
+#else
   const char *CONFIG_PATH = "/.config/simpleboard/simpleboard.conf";
   const char *HOME = getenv("HOME");
   char path[100];
@@ -27,44 +27,28 @@ FILE *getConfigFile() {
   strcpy(path + strlen(path), CONFIG_PATH);
 
   return fopen(path, "r");
+#endif
 }
 
 WINDOW *createDisplay(Config *config) {
-  int height = 10;
-  int width = 20;
-  int dynamicHeight = 0;
-  int dynamicWidth = 0;
-  int dynamicWidthMin = 0;
+  int height = config->preference.height;
+  int width = config->preference.width;
+  int dynamicHeight = config->preference.dynamicHeight;
+  int dynamicWidth = config->preference.dynamicWidth;
+  int dynamicWidthMin = config->preference.dynamicWidthMin;
   int startx, starty;
 
-  for (int i = 0; i < config->preferenceCount; i++) {
-    if (!strcmp(config->preference[i].key, "width")) {
-      width = atoi(config->preference[i].value);
-    } else if (!strcmp(config->preference[i].key, "height")) {
-      height = atoi(config->preference[i].value);
-    } else if (!strcmp(config->preference[i].key, "dynamicHeight")) {
-      dynamicHeight = atoi(config->preference[i].value);
-    } else if (!strcmp(config->preference[i].key, "dynamicWidth")) {
-      dynamicWidth = atoi(config->preference[i].value);
-    } else if (!strcmp(config->preference[i].key, "dynamicWidthMin")) {
-      dynamicWidthMin = atoi(config->preference[i].value);
-    }
-  }
   if (dynamicHeight) {
-    height = config->commandCount + 2;
+    height = config->command.count + 2;
   }
   if (dynamicWidth) {
     int maxWidth = dynamicWidthMin;
-    for (int i = 0; i < config->preferenceCount; i++) {
-      if (!strcmp(config->preference[i].key, "titleOverride")) {
-        int len = strlen(config->preference[i].value);
-        if (len > maxWidth) {
-          maxWidth = len;
-        }
-      }
+    int len = strlen(config->preference.title);
+    if (len > maxWidth) {
+      maxWidth = len;
     }
-    for (int i = 0; i < config->commandCount; i++) {
-      int len = strlen(config->command[i].name);
+    for (int i = 0; i < config->command.count; i++) {
+      int len = strlen(config->command.commands[i].name);
       if (len > maxWidth) {
         maxWidth = len;
       }
@@ -83,60 +67,44 @@ void setPreference(Config *config, WINDOW *display) {
   init_pair(1, COLOR_RED, -1);
   init_pair(2, COLOR_BLUE, -1);
 
-  for (int i = 0; i < config->preferenceCount; i++) {
-    if (!strcmp(config->preference[i].key, "color")) {
-      if (!strcmp(config->preference[i].value, "blue")) {
-        init_pair(2, COLOR_BLUE, -1);
-      } else if (!strcmp(config->preference[i].value, "red")) {
-        init_pair(2, COLOR_RED, -1);
-      } else if (!strcmp(config->preference[i].value, "green")) {
-        init_pair(2, COLOR_GREEN, -1);
-      } else if (!strcmp(config->preference[i].value, "magenta")) {
-        init_pair(2, COLOR_MAGENTA, -1);
-      }
-    } else if (!strcmp(config->preference[i].key, "titleColor")) {
-      if (!strcmp(config->preference[i].value, "blue")) {
-        init_pair(1, COLOR_BLUE, -1);
-      } else if (!strcmp(config->preference[i].value, "red")) {
-        init_pair(1, COLOR_RED, -1);
-      } else if (!strcmp(config->preference[i].value, "green")) {
-        init_pair(1, COLOR_GREEN, -1);
-      } else if (!strcmp(config->preference[i].value, "magenta")) {
-        init_pair(1, COLOR_MAGENTA, -1);
-      }
-    } else if (!strcmp(config->preference[i].key, "title")) {
-      title = atoi(config->preference[i].value);
-    } else if (!strcmp(config->preference[i].key, "titleOverride")) {
-      titleOverride = config->preference[i].value;
-    } else if (!strcmp(config->preference[i].key, "border")) {
-      if (!strcmp(config->preference[i].value, "0")) {
-        wborder(display, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-      }
-    }
+  if (!strcmp(config->preference.color, "blue")) {
+    init_pair(2, COLOR_BLUE, -1);
+  } else if (!strcmp(config->preference.color, "red")) {
+    init_pair(2, COLOR_RED, -1);
+  } else if (!strcmp(config->preference.color, "green")) {
+    init_pair(2, COLOR_GREEN, -1);
+  } else if (!strcmp(config->preference.color, "magenta")) {
+    init_pair(2, COLOR_MAGENTA, -1);
+  }
+  if (!strcmp(config->preference.titleColor, "blue")) {
+    init_pair(1, COLOR_BLUE, -1);
+  } else if (!strcmp(config->preference.titleColor, "red")) {
+    init_pair(1, COLOR_RED, -1);
+  } else if (!strcmp(config->preference.titleColor, "green")) {
+    init_pair(1, COLOR_GREEN, -1);
+  } else if (!strcmp(config->preference.titleColor, "magenta")) {
+    init_pair(1, COLOR_MAGENTA, -1);
+  }
+  if (config->preference.border == 0) {
+    wborder(display, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
   }
 }
 
 void printOptions(Config *config, WINDOW *display) {
-  if (title) {
-    wattron(display, COLOR_PAIR(1));
-    if (titleOverride) {
-      mvwprintw(display, 0, 1, "%s", titleOverride);
-    } else {
-      mvwprintw(display, 0, 1, "simpleboard");
-    }
-  }
+  wattron(display, COLOR_PAIR(1));
+  mvwprintw(display, 0, 1, "%s", config->preference.title);
   wattron(display, COLOR_PAIR(2));
-  for (int i = 0; i < config->commandCount; i++) {
-    mvwprintw(display, i + 1, 1, "%s", config->command[i].name);
+  for (int i = 0; i < config->command.count; i++) {
+    mvwprintw(display, i + 1, 1, "%s", config->command.commands[i].name);
   }
   refresh();
   wrefresh(display);
 }
 
 CommandEntry *getCommand(Config *config, char key) {
-  for (int i = 0; i < config->commandCount; i++) {
-    if (config->command[i].hotkey[0] == key) {
-      return config->command + i;
+  for (int i = 0; i < config->command.count; i++) {
+    if (config->command.commands[i].hotkey[0] == key) {
+      return config->command.commands + i;
     }
   }
   return NULL;
@@ -144,7 +112,7 @@ CommandEntry *getCommand(Config *config, char key) {
 
 int main() {
   FILE *fp = getConfigFile();
-  Config config;
+  INIT_CONIFG(config);
   if (readConfig(fp, &config)) {
     puts("readConfig: invalid file pointer, does the config file exist?");
     return EXIT_FAILURE;
@@ -161,15 +129,13 @@ int main() {
 
 #if DEBUG
   printw("Commands:\n");
-  for (int i = 0; i < config.commandCount; i++) {
-    printw("%s = %s, %s, %s\n", config.command[i].name,
-           config.command[i].command, config.command[i].hotkey,
-           config.command[i].misc);
+  for (int i = 0; i < config.command.count; i++) {
+    printw("%s = %s, %s, %s\n", config.command.commands[i].name,
+           config.command.commands[i].command,
+           config.command.commands[i].hotkey, config.command.commands[i].misc);
   }
   printw("Preferences:\n");
-  for (int i = 0; i < config.preferenceCount; i++) {
-    printw("%s = %s\n", config.preference[i].key, config.preference[i].value);
-  }
+  printw("%s", config.preference.title);
   refresh();
   getch();
   clear();
